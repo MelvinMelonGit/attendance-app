c662924a43c88fe0b757374ddfae646a12c02cf2
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2024-07-20 16:16:59 +0800

    configured primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Panel from "primevue/panel";
+import Lara from "./presets/lara";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component("Panel", Panel);
+app.use(PrimeVue, { unstyled: true, pt: Lara });
+app.mount("#app");
